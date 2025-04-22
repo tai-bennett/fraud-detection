8b37b5b35bdbc745e1bef3f2314aa2d7a63f9cf3
tai-bennett <bennettduncantai@gmail.com>
2025-04-21 17:06:14 -0700

    initial exploration

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@
 python3 -m venv ./venv &&
 source ./venv/bin/activate &&
 python -m pip install pip --upgrade &&
-python -m pip install pandas, numpy, matplotlib &&
-deativate
+python -m pip install pandas numpy matplotlib &&
+python -m pip install kaggle &&
+python -m pip install jupyter &&
+deactivate
